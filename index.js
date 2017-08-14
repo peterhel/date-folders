072e102e8ee7df15f9056ca35288475f0f65ba9c
Peter Helenefors <peterhelenefors@Peters-MacBook-Pro.local>
2017-08-14 14:07:58 +0200

    method extracted to enable exif option

--- index.js
+++ index.js
@@ -1,39 +1,60 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-
+const ExifImage = require('exif').ExifImage;
 const [, , flag] = process.argv;
 
-const mvinst = {};
-const files = fs.readdirSync(process.cwd());
-
-files.forEach(file => {
+async function getStatTimestamp(file) {
   const stat = fs.statSync(file);
   if(!stat.isFile()) {
-    return;
+    return Promise.reject({
+      error: 'Is not file'
+    });
   }
-
+  
   const birth = new Date(stat.birthtime);
   const year = `${birth.getFullYear()}`;
   const month = `0${birth.getMonth() + 1}`.slice(-2);
   const date = `0${birth.getDate()}`.slice(-2);
+  
+  return Promise.resolve({
+    year,
+    month,
+    date
+  });
+}
 
-  if(flag !== '--execute') {
-    console.log(`"${file}" "${year}/${month}/${date}/"`);
-    return;
-  }
-
-  if(!fs.existsSync(year)) {
-    fs.mkdirSync(year);
-  }
-
-  if(!fs.existsSync(`${year}/${month}`)) {
-    fs.mkdirSync(`${year}/${month}`);
-  }
-
-  if(!fs.existsSync(`${year}/${month}/${date}`)) {
-    fs.mkdirSync(`${year}/${month}/${date}`);
-  }
-
-  fs.renameSync(file, `${year}/${month}/${date}/${file}`)
-});
+(async function run() {
+  const files = fs.readdirSync(process.cwd());
+  
+  files.forEach(async (file) => {
+    let year, month, date;
+    try {
+      const timestamp = await getStatTimestamp(file);
+      year = timestamp.year;
+      month = timestamp.month;
+      date = timestamp.date;
+    } catch (nofile) {
+      return;
+    }
+    
+    if(flag !== '--execute') {
+      console.log(`"${file}" "${year}/${month}/${date}/"`);
+      return;
+    }
+    
+    if(!fs.existsSync(year)) {
+      fs.mkdirSync(year);
+    }
+    
+    if(!fs.existsSync(`${year}/${month}`)) {
+      fs.mkdirSync(`${year}/${month}`);
+    }
+    
+    if(!fs.existsSync(`${year}/${month}/${date}`)) {
+      fs.mkdirSync(`${year}/${month}/${date}`);
+    }
+    
+    fs.renameSync(file, `${year}/${month}/${date}/${file}`)
+  });
+})();
